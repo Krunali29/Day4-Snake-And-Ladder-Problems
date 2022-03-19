fea7160d32c1fb1fea944e69431af668d93f0334
Krunali <krunalilole29@gmail.com>
2022-03-20 01:23:09 +0530

    UC1 Start Position Added

---
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+//Day 4 Snake And Ladder Simulator
+public class SnakeAndLadder
+{
+    public static void main(String[] args)
+    {
+
+        System.out.println("---------- Welcome to the Game of SNAKES AND LADDERS!! ----------");
+
+        int startPositionA = 0;
+
+    }
+}
